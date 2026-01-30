4fee464605a15630d2eda509a397db549803845e
Rahul Bhadani <kumarrahulbhadani@gmail.com>
2026-01-29 18:29:55 -0600

    init file updated

--- src/cpe487587hw01/__init__.py
+++ src/cpe487587hw01/__init__.py
@@ -1,5 +1,5 @@
-from cpe487587hw01._core import hello_from_bin
+#from cpe487587hw01._core import hello_from_bin
 from .deepl import binary_classification
 
-def hello() -> str:
-    return hello_from_bin()
+#def hello() -> str:
+#    return hello_from_bin()
